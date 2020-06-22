17116d4c51874cee57713b12514e5c7776fdf25d
Samuel Eding <smleding28@gmail.com>
2020-06-22 16:48:18 -0400

    fix all the units tests

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,19 +1,37 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HeaderComponent } from './header.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Store } from '@ngrx/store';
+import { provideMockStore } from '@ngrx/store/testing';
+import {
+  initialState as BrokerState, MODULE_NAME as BROKER_MODULE, BrokerEffects
+} from '_@core/broker';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let store: Store<any>;
+  const initialState = { [BROKER_MODULE]: BrokerState };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [ HeaderComponent ],
+      providers: [
+        BrokerEffects,
+        provideMockStore({
+            initialState,
+        }),
+      ],
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    store = TestBed.get(Store);
+    spyOn(store, 'dispatch').and.callThrough();
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
